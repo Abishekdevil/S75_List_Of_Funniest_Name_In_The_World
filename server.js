bc026a10787bc71cdfe66e833c726f4d64b8ba69
Abishekdevil <abishekcholan07@gmail.com>
2025-02-14 10:58:32 +0530

    updated

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const app = express();
-const PORT = 5000;
+require("dotenv").config();
+const Mongoose = require("mongoose");
+const PORT = 3001; // Not nes
 
-app.get("/ping",(req,res)=>{
+app.get("/ping", (req, res)=>{
     res.send("pong")
 });
 
-app.listen(PORT,()=>{
+const MONGO_URI = process.env.uri;
+Mongoose.connect(MONGO_URI)
+.then(()=> console.log("Database connected successfully"))
+.catch((err)=>{
+    console.log("Error found:",err)
+})
+
+app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
